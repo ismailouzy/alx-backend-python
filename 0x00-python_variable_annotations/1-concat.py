f66da1982d15540879ca2091232a8bdfd505145d
ismailouzy <ismailouzal@gmail.com>
2024-08-01 21:28:26 +0000

    d

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+a typed-annotation function CONCAT
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    a function that concatenates two strings
+    """
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function FLOOR
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    the floor of a float number
+    """
+    return math.floor(n)
