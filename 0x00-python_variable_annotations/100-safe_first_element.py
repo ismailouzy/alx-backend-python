ae3c306d4d15d69bb46952fa3840c8f3f03213ab
ismailouzy <ismailouzal@gmail.com>
2024-08-01 23:53:59 +0000

    fixed pycode 100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -7,7 +7,8 @@ from typing import Sequence, Any, Union, Optional
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     """
-    This function takes a sequence and returns its first element if the sequence
+    This function takes a sequence and returns
+    its first element if the sequence
     is not empty. If the sequence is empty, it returns None.
     """
     if lst:
