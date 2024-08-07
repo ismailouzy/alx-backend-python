5faf4616b8f5161933a9ee6ee3ea8bf8bb40757c
ismailouzy <ismailouzal@gmail.com>
2024-08-07 19:38:19 +0000

    changed the Type annotation to generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,13 +4,13 @@ async_generator
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     An asynchronous generator that yields random numbers.
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+        yield random.uniform(0, 10)
