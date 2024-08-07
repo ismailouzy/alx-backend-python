5aa172f7efc1256c1a7bf59c7c06a4539656184e
ismailouzy <ismailouzal@gmail.com>
2024-08-07 19:29:45 +0000

    d

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+async_generator
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    An asynchronous generator that yields random numbers.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
