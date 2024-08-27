0fec9bd6d0b19248ec14df185d6b4eaa0595dbc4
ismailouzy <ismailouzal@gmail.com>
2024-08-27 20:52:55 +0000

    d

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
-"""Unit tests for utils module."""
+"""Unit tests for utils.access_nested_map function."""
 
 import unittest
-from unittest.mock import patch, Mock
 from parameterized import parameterized
-from typing import Any, Dict, Tuple, Union
-from utils import access_nested_map, get_json, memoize
+from typing import Dict, Union, Tuple
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """Tests for access_nested_map function."""
 
-    @parameterized.expand([({"a": 1}, ("a",), 1), ({"a": {
-        "b": 2}}, ("a",), {"b": 2}), ({"a": {"b": 2}}, ("a", "b"), 2)])
-    def test_access_nested_map(
-            self, nested_map: Dict,
-            path: Tuple[str],
-            expected: Union[Dict, int],
-            ) -> None:
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map: Dict, path: Tuple[str], expected: Union[Dict, int]) -> None:
         """Test access_nested_map returns correct output."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
-
-    @parameterized.expand([({}, ("a",)), ({"a": 1}, ("a", "b"))])
